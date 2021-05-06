2e82b78b6710fa71b0a6b9643f6d84f5819b46c2
klamb95 <klamb1995@gmail.com>
2021-05-06 16:18:18 +0100

    first commit

---
+++ lab_events_start/js/app.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('JavaScript loaded');
+  
+  
+  // function addButton () {
+  //   const newDeleteAllButton = document.createElement(`Button`);
+  //   newDeleteAllButton.setAttribute("id", "delete");
+  //   newDeleteAllButton.textContent = 'Delete All';
+  //   const currentDiv = document.getElementById("test");
+  //   document.body.insertBefore(newDeleteAllButton, currentDiv);
+  // };
+
+  const button = document.querySelector('#delete');
+  button.addEventListener('click', handleDeleteButton);
+
+  const form = document.querySelector('#new-item-form');
+  form.addEventListener('submit', handleFormSubmit);
+
+});
+
+const handleFormSubmit = function (event) {
+  event.preventDefault();
+  const newListItem = document.createElement('li');
+    newListItem.textContent = `Title: ${event.target.title.value} Author: ${event.target.author.value} Category: ${event.target.category.value}`
+
+    const list = document.querySelector('ul');
+    list.appendChild(newListItem);
+
+    document.getElementById("new-item-form").reset();
+};
+
+const handleDeleteButton = function () {
+  document.getElementById("reading-list").innerHTML = "";
+}
+
+  
